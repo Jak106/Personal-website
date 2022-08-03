9c50e360f8b653bcb01909347cbcd32cb8727ec8
Jak106 <jakubzuebr2004@gmail.com>
2022-08-03 20:20:51 +0200

    settings

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -121,10 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+#    BASE_DIR / "static",
 ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
